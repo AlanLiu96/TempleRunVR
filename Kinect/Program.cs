2fe04d7cfe023b32e6310e28b77daeb785315c9f
ShreyasTirumala <shreyas.tirumala@gmail.com>
2015-11-07 03:11:02 -0500

    Initial commit, basic tests with Kinect

---
+++ Kinect/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+using Microsoft.Kinect;
+
+namespace JumpAndRun
+{
+    public static class Program
+    {
+        static KinectSensor kinect = null;
+        static BodyFrameReader bfr = null;
+        static Body[] bodies = null;
+
+        static void arrHandle(object sender, EventArgs e)
+        {
+            Console.WriteLine("Frame received");
+        }
+        static void Main(string[] args)
+        {
+            kinect = KinectSensor.GetDefault();
+            
+            bfr = kinect.BodyFrameSource.OpenReader();
+            bfr.FrameArrived += arrHandle;
+            kinect.Open();
+            while(true)
+            {
+                /*if (kinect.IsAvailable)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Available!");
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Unavailable!");
+                }*/
+            }
+        }
+    }
+}
